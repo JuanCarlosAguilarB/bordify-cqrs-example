52412734fb0556954a4c2f401280d9f24ccf07fc
JuanC <abjuancarlos.12@gmail.com>
2024-07-12 01:06:14 -0500

    feat: controller for to get users was created.

--- src/main/java/com/bordify/user/infrastructure/controllers/UserGetController.java
+++ src/main/java/com/bordify/user/infrastructure/controllers/UserGetController.java
@@ -1,13 +1,17 @@
 package com.bordify.user.infrastructure.controllers;
 
 
+import com.bordify.shared.domain.PageResult;
+import com.bordify.shared.domain.PaginationRequest;
 import com.bordify.user.application.find.UserFinder;
 import com.bordify.user.domain.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User", description = "User management operations")
@@ -28,5 +32,16 @@ public class UserGetController {
         return user;
 
     }
+
+    @Operation(summary = "Get all users", description = "Get all users", tags = { "User" })
+    @GetMapping(value = "/v1/users/")
+    public ResponseEntity<PageResult<User>> retriveUsers(@RequestParam PaginationRequest pagination){
+
+        PageResult<User> userList = userServices.getall(pagination);
+
+        return ResponseEntity.ok(userList);
+
+    }
+
 
 }
