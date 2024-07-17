85b50671e82cd64fed04bfca0691592d4c31e04d
JuanC <abjuancarlos.12@gmail.com>
2024-07-17 15:42:37 -0500

    chore: optimizing imports

--- src/main/java/com/bordify/user/infrastructure/persistence/UserEntity.java
+++ src/main/java/com/bordify/user/infrastructure/persistence/UserEntity.java
@@ -1,6 +1,9 @@
 package com.bordify.user.infrastructure.persistence;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 import java.time.LocalTime;
