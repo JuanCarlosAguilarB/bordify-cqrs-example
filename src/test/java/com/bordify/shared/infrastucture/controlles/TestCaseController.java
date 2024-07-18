124fe8b113b4c5ec73b53b6f6713eeda243909d1
JuanC <abjuancarlos.12@gmail.com>
2024-07-18 03:09:49 -0500

    fix: put correct message for users not unauthenticated.

--- src/test/java/com/bordify/shared/infrastucture/controlles/TestCaseController.java
+++ src/test/java/com/bordify/shared/infrastucture/controlles/TestCaseController.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
