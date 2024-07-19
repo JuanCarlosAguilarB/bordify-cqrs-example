5211d76e02d3f4d1fd18b1f6bcc4084c534b3bb4
JuanC <abjuancarlos.12@gmail.com>
2024-07-19 04:07:58 -0500

    feat: create model UserAuthInformation. The idea is to have information of user in model User.

---
+++ src/main/java/com/bordify/auth/domain/UserAuthInformation.java
@@ -0,0 +1,22 @@
+package com.bordify.auth.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class UserAuthInformation {
+
+    private UUID        userId;
+    private String      username;
+    private String      password;
+    private Boolean     isVerified;
+    private LocalTime   created;
+    private LocalTime   lastLogin;
+
+}
