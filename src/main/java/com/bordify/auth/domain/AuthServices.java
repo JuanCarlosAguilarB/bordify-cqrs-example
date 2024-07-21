26f6c328ee59542043aac3195bb96d935d5f0044
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 17:16:20 -0500

    feat: create method in AuthService to decode token.

--- src/main/java/com/bordify/auth/domain/AuthServices.java
+++ src/main/java/com/bordify/auth/domain/AuthServices.java
@@ -6,6 +6,7 @@ import com.bordify.shared.domain.CreadentialsNotValidException;
 public interface AuthServices {
 
     public AuthenticationToken createToken(UserAuthInformation user);
+    public UserAuthInformation decode(AuthenticationToken token);
     public void ensureCredentialsAreValid(Auth auth) throws CreadentialsNotValidException;
 
 }
