d3d70911a4ebe128cde122bc7f0f88fd8d0ea9a4
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 05:20:36 -0500

    feat: create controller to create UserAuthInformation model.

---
+++ src/main/java/com/bordify/auth/infrastructure/controllers/AuthPutController.java
@@ -0,0 +1,54 @@
+package com.bordify.auth.infrastructure.controllers;
+
+import com.bordify.auth.application.create.UserAuthInformationCreator;
+import com.bordify.auth.domain.UserAuthInformation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@Tag(name = "Auth", description = "Authentication operations")
+@AllArgsConstructor
+public class AuthPutController {
+
+    private final UserAuthInformationCreator userServices;
+
+    @PutMapping ("/v1/signup/{userId}/")
+    public ResponseEntity<Map<String, Object>> signUpUser(@RequestBody UserAuthInformationRequest userRequest, @PathVariable UUID userId) {
+
+        UserAuthInformation userAuthInformation = UserAuthInformation.builder()
+                .userId(userId)
+                .email(userRequest.getEmail())
+                .username(userRequest.getUsername())
+                .password(userRequest.getPassword())
+                .isVerified(false)
+                .created(LocalTime.now())
+                .lastLogin(null)
+                .build();
+
+        userServices.createUser(userAuthInformation);
+
+        return ResponseEntity.ok(Map.of("message", "User created successfully"));
+    }
+
+}
+
+
+@Builder
+@Data
+class UserAuthInformationRequest {
+
+    private String      email;
+    private String      username;
+    private String      password;
+}
