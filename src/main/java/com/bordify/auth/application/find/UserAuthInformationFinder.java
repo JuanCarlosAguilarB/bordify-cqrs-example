b92482d47754b4707ac768504ead3205acbbc583
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 05:45:38 -0500

    feat: create service to find an UserAuthInformation by username.

---
+++ src/main/java/com/bordify/auth/application/find/UserAuthInformationFinder.java
@@ -0,0 +1,27 @@
+package com.bordify.auth.application.find;
+
+import com.bordify.auth.domain.UserAuthInformation;
+import com.bordify.auth.domain.UserAuthInformationRepository;
+import com.bordify.user.domain.UserNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserAuthInformationFinder {
+
+    private final UserAuthInformationRepository userRepository;
+
+    /**
+     * Retrieves a user by the specified username.
+     *
+     * @param username The username of the user to retrieve.
+     * @return The User object corresponding to the specified username.
+     * @throws UserNotFoundException If no user is found with the specified username.
+     */
+    public UserAuthInformation findUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User not found with username: " + username));
+    }
+
+}
