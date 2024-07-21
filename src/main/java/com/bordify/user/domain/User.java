8136238233261e475b38dc233656e05e15c5cdbf
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 17:53:29 -0500

    feat: modify model user because of new feature. Now, the project is broken.

--- src/main/java/com/bordify/user/domain/User.java
+++ src/main/java/com/bordify/user/domain/User.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
-import java.time.LocalTime;
 import java.util.UUID;
 
 
@@ -16,14 +15,8 @@ import java.util.UUID;
 public class User {
 
     private UUID        id;
-    private String      username;
-    private String      password;
-    private String      email;
     private String      firstName;
     private String      lastName;
-    private Boolean     isVerified;
     private String      phoneNumber;
-    private LocalTime   created;
-    private LocalTime   lastLogin;
-
+    private String      username;
 }
