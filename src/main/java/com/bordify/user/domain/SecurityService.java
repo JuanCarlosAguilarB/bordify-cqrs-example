14ae606b96be4fa0dd5e84ba619dec6f66ba5b32
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 05:41:40 -0500

    feat: create specification to ask that two texts are similar

--- src/main/java/com/bordify/user/domain/SecurityService.java
+++ src/main/java/com/bordify/user/domain/SecurityService.java
@@ -3,5 +3,6 @@ package com.bordify.user.domain;
 public interface SecurityService {
 
     public String encode(String textToEncode);
+    public Boolean matches(String textToEncode, String encodedText);
 
 }
