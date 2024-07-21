68845ed5d187f55b6ea28527f1c08d4fe5ec272d
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 05:38:35 -0500

    feat: create specification to ensure that credentials must be validated.

--- src/main/java/com/bordify/auth/domain/AuthServices.java
+++ src/main/java/com/bordify/auth/domain/AuthServices.java
@@ -1,8 +1,12 @@
 package com.bordify.auth.domain;
 
 
+import com.bordify.shared.domain.CreadentialsNotValidException;
+
 public interface AuthServices {
 
     public AuthenticationToken createToken(UserAuthInformation user);
     public void authenticate(Auth auth);
+    public void ensureCredentialsAreValid(Auth auth) throws CreadentialsNotValidException;
+
 }
