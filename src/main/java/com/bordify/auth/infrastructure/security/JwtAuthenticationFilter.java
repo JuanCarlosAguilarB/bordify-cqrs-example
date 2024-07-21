fb892436ae878fc1380ed419336e734ef3275feb
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 17:29:38 -0500

    chore:  remove unused methods in Jwt Filter

--- src/main/java/com/bordify/auth/infrastructure/security/JwtAuthenticationFilter.java
+++ src/main/java/com/bordify/auth/infrastructure/security/JwtAuthenticationFilter.java
@@ -1,8 +1,5 @@
 package com.bordify.auth.infrastructure.security;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.bordify.auth.domain.AuthServices;
 import com.bordify.auth.domain.AuthenticationToken;
 import com.bordify.auth.domain.UserAuthInformation;
@@ -12,7 +9,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,11 +16,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.UUID;
 
 /**
@@ -93,53 +87,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    /**
-     * Extracts the JWT token from the request headers.
-     *
-     * @param request The HTTP servlet request.
-     * @return The JWT token if found, null otherwise.
-     */
-    private String getTokenFromRequest(HttpServletRequest request) {
-        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-
-        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
-            return authHeader.substring(7);
-        }
-        return null;
-    }
-
-    /**
-     * Extracts the username from the provided JWT token.
-     *
-     * @param token The JWT token from which to extract the username.
-     * @return The username extracted from the token.
-     */
-    public String getUsernameFromToken(String token) {
-        DecodedJWT decodedJWT = JWT.decode(token);
-        return decodedJWT.getSubject();
-    }
-
-    /**
-     * Validates whether the provided JWT token is valid for the given user details.
-     *
-     * @param token The JWT token to validate.
-     * @param userDetails The user details against which to validate the token.
-     * @return True if the token is valid for the given user details, false otherwise.
-     */
-    public Boolean isValidToken(String token, UserDetails userDetails) {
-        try {
-            DecodedJWT decodedJWT = JWT.decode(token);
-
-            LocalDate now = LocalDate.now();
-
-            return !decodedJWT.getExpiresAt().before(java.sql.Date.valueOf(now)) ||
-                    decodedJWT.getSubject().equals(userDetails.getUsername());
-
-        } catch (JWTDecodeException exception) {
-            return false;
-        }
-    }
 }
-
-
-
