cc478ca258cf79b6f1142c987686047c6a5bd367
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 18:04:26 -0500

    feat: adjusted service to create a new user
    ** The project is broken still.

--- src/main/java/com/bordify/user/application/create/UserCreator.java
+++ src/main/java/com/bordify/user/application/create/UserCreator.java
@@ -1,9 +1,9 @@
 package com.bordify.user.application.create;
 
 import com.bordify.user.domain.DuplicateEmailException;
-import com.bordify.user.domain.SecurityService;
 import com.bordify.user.domain.User;
 import com.bordify.user.domain.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
@@ -12,16 +12,10 @@ import org.springframework.stereotype.Service;
  * Service class for user-related operations.
  */
 @Service
+@AllArgsConstructor
 public class UserCreator {
 
     private final UserRepository userRepository;
-    private final SecurityService securityService;
-
-    public UserCreator(UserRepository userRepository, SecurityService securityService) {
-        this.userRepository = userRepository;
-        this.securityService = securityService;
-    }
-
 
     /**
      * Creates a new user with the provided user details and generates an authentication token.
@@ -31,15 +25,6 @@ public class UserCreator {
      */
     public void createUser(User user) {
 
-        if (userRepository.existsByEmail(user.getEmail()) && userRepository.existsByUsername(user.getUsername())) {
-            throw new DuplicateEmailException("Both the email " + user.getEmail() + " and username " + user.getUsername() + " are already registered. Please use different credentials.");
-        } else if (userRepository.existsByEmail(user.getEmail())) {
-            throw new DuplicateEmailException("The email " + user.getEmail() + " is already registered. Please use a different email.");
-        } else if (userRepository.existsByUsername(user.getUsername())) {
-            throw new DuplicateEmailException("The username " + user.getUsername() + " is already registered. Please use a different username.");
-        }
-
-        user.setPassword(securityService.encode(user.getPassword()));
         userRepository.save(user);
 
     }
