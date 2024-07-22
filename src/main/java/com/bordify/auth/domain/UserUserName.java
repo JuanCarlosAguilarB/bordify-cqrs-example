bf483ee8591ed55d95e2675a7b9165c628fb3974
JuanC <abjuancarlos.12@gmail.com>
2024-07-21 22:59:28 -0500

    feat: create value objects for Auth domain model

---
+++ src/main/java/com/bordify/auth/domain/UserUserName.java
@@ -0,0 +1,21 @@
+package com.bordify.auth.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+public final class UserUserName {
+
+
+    private final String value;
+
+    public UserUserName(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+}
