280c20a11a7943097001c7ab4345c3788d2b5595
JuanC <abjuancarlos.12@gmail.com>
2024-07-25 02:44:26 -0500

    feat: create command to create a new UserAuthInformation

---
+++ src/main/java/com/bordify/auth/domain/CreateUserAuthInformationCommand.java
@@ -0,0 +1,19 @@
+package com.bordify.auth.domain;
+
+import com.bordify.shared.domain.Command;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+
+@Builder
+@Data
+public class CreateUserAuthInformationCommand implements Command {
+
+    private UUID userId;
+    private String email;
+    private String username;
+    private String password;
+
+}
