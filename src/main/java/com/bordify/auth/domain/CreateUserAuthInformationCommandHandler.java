28a81bcb58170cb3ba22077587f1b2a114ad1b9a
JuanC <abjuancarlos.12@gmail.com>
2024-07-25 02:48:22 -0500

    feat: firts approach to resolve the problem of mapping commands with handlers.
    # work in progress.

---
+++ src/main/java/com/bordify/auth/domain/CreateUserAuthInformationCommandHandler.java
@@ -0,0 +1,38 @@
+package com.bordify.auth.domain;
+
+import com.bordify.auth.application.create.UserAuthInformationCreator;
+import com.bordify.shared.domain.CommandHandler;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+
+@Builder
+@Data
+@Service("CreateUserAuthInformationCommandHandler")
+@AllArgsConstructor
+public class CreateUserAuthInformationCommandHandler implements CommandHandler<CreateUserAuthInformationCommand> {
+
+    private final UserAuthInformationCreator service;
+
+    @Override
+    public Class<CreateUserAuthInformationCommand> getCommandType() {
+        return null;
+    }
+
+    @Override
+    public void handle(CreateUserAuthInformationCommand command) {
+        System.out.println("CreateUserAuthInformationCommandHandler");
+
+        UserAuthInformation user = UserAuthInformation.builder()
+                .userId(command.getUserId())
+                .email(command.getEmail())
+                .username(command.getUsername())
+                .password(command.getPassword())
+                .build();
+
+        service.createUser(user);
+    }
+
+}
--- src/main/java/com/bordify/shared/infrastructure/bus/CommandBusAdaptaer.java
+++ src/main/java/com/bordify/shared/infrastructure/bus/CommandBusAdaptaer.java
@@ -1,13 +1,38 @@
 package com.bordify.shared.infrastructure.bus;
 
+import com.bordify.auth.domain.CreateUserAuthInformationCommand;
 import com.bordify.shared.domain.Command;
 import com.bordify.shared.domain.CommandBus;
+import com.bordify.shared.domain.CommandHandler;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class CommandBusAdaptaer implements CommandBus {
+
+    private final ApplicationContext context;
+    Map<Class<? extends Command>, String> handlers = new HashMap<>();
+
+    public CommandBusAdaptaer(ApplicationContext context) {
+        this.context = context;
+    }
 
 
     @Override
     public void send(Command command) {
+        handlers.put(CreateUserAuthInformationCommand.class, "CreateUserAuthInformationCommandHandler");
 
+        CommandHandler handler = (CommandHandler) context.getBean(handlers.get(command.getClass()));
+
+        if (handler != null) {
+            try {
+                handler.handle(command);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
