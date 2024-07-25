a769ee36c58b73812781638f81e47b43f32fdad3
JuanC <abjuancarlos.12@gmail.com>
2024-07-25 01:35:37 -0500

    chore: create implementation dummy of the interface CommandBus

---
+++ src/main/java/com/bordify/shared/infrastructure/bus/CommandBusAdaptaer.java
@@ -0,0 +1,13 @@
+package com.bordify.shared.infrastructure.bus;
+
+import com.bordify.shared.domain.Command;
+import com.bordify.shared.domain.CommandBus;
+
+public class CommandBusAdaptaer implements CommandBus {
+
+
+    @Override
+    public void send(Command command) {
+
+    }
+}
