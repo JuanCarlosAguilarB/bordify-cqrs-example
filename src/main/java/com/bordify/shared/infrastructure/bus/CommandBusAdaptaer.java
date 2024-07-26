9fdbed73eb0704c9207570eae39b8ca9e2999e49
JuanC <abjuancarlos.12@gmail.com>
2024-07-26 00:03:46 -0500

    feat: added approach of commandBus.
    # project works satisfactory

--- src/main/java/com/bordify/shared/infrastructure/bus/CommandBusAdaptaer.java
+++ src/main/java/com/bordify/shared/infrastructure/bus/CommandBusAdaptaer.java
@@ -1,38 +1,35 @@
 package com.bordify.shared.infrastructure.bus;
 
-import com.bordify.auth.domain.CreateUserAuthInformationCommand;
 import com.bordify.shared.domain.Command;
 import com.bordify.shared.domain.CommandBus;
 import com.bordify.shared.domain.CommandHandler;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Service
 public class CommandBusAdaptaer implements CommandBus {
 
-    private final ApplicationContext context;
-    Map<Class<? extends Command>, String> handlers = new HashMap<>();
-
-    public CommandBusAdaptaer(ApplicationContext context) {
-        this.context = context;
+    private final ApplicationContext applicationContext;
+    private Map< Class<? extends Command>, CommandHandler> commandHandlerMap = new HashMap<>();
+    public CommandBusAdaptaer(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
     }
 
+   @PostConstruct
+    private void registerCommandHandlers() {
+       applicationContext.getBeansOfType(CommandHandler.class).forEach((key, value) -> {
+           commandHandlerMap.put(value.getCommandType(), value);
+       });
+    }
 
     @Override
     public void send(Command command) {
-        handlers.put(CreateUserAuthInformationCommand.class, "CreateUserAuthInformationCommandHandler");
-
-        CommandHandler handler = (CommandHandler) context.getBean(handlers.get(command.getClass()));
-
-        if (handler != null) {
-            try {
-                handler.handle(command);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        CommandHandler commandHandler = commandHandlerMap.get(command.getClass());
+        commandHandler.handle(command);
     }
 }
