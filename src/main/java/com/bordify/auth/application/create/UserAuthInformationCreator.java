66067e6c0797d487802dc9e12b8e768fc7fd4e73
JuanC <abjuancarlos.12@gmail.com>
2024-07-27 21:34:51 -0500

    feat: UserAuthInformationCreator is changed, now the object values are passed to the service.     # problem: the project dont works bc of the value objects

--- src/main/java/com/bordify/auth/application/create/UserAuthInformationCreator.java
+++ src/main/java/com/bordify/auth/application/create/UserAuthInformationCreator.java
@@ -1,11 +1,13 @@
 package com.bordify.auth.application.create;
 
-import com.bordify.auth.domain.UserAuthInformation;
-import com.bordify.auth.domain.UserAuthInformationRepository;
+import com.bordify.auth.domain.*;
+import com.bordify.shared.domain.UserUserId;
 import com.bordify.user.domain.DuplicateEmailException;
 import com.bordify.user.domain.SecurityService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
@@ -18,22 +20,41 @@ public class UserAuthInformationCreator {
     /**
      * Creates a new user with the provided user details and generates an authentication token.
      *
-     * @param user The user object containing the user details to be created.
      * @throws DuplicateEmailException If the provided email or username is already registered.
      */
-    public void createUser(UserAuthInformation user) {
+    public void createUser(UserUserId id, UserEmail email, UserUserName userName, UserPassword password) {
 
-        if (userRepository.existsByEmail(user.getEmail()) && userRepository.existsByUsername(user.getUsername())) {
-            throw new DuplicateEmailException("Both the email " + user.getEmail() + " and username " + user.getUsername() + " are already registered. Please use different credentials.");
-        } else if (userRepository.existsByEmail(user.getEmail())) {
-            throw new DuplicateEmailException("The email " + user.getEmail() + " is already registered. Please use a different email.");
-        } else if (userRepository.existsByUsername(user.getUsername())) {
-            throw new DuplicateEmailException("The username " + user.getUsername() + " is already registered. Please use a different username.");
-        }
+        ensureEmailAndUserNameAreNotRegistered(email, userName);
 
-        user.setPassword(securityService.encode(user.getPassword()));
+        String rawPassword = password.value();
+        UserPassword passwordEncoded = new UserPassword(securityService.encode(rawPassword));
+
+        UserAuthInformation user = new UserAuthInformation(
+                id,
+                email,
+                userName,
+                passwordEncoded,
+                new UserIsVerified(false),
+                new UserDateCreated(LocalDateTime.now()),
+                new UserDateLastLogin(LocalDateTime.now())
+                );
+
         userRepository.save(user);
 
+    }
+
+    private void ensureEmailAndUserNameAreNotRegistered(UserEmail email, UserUserName userName) {
+
+        boolean isEmailRegistered = userRepository.existsByEmail(email);
+        boolean isUserNameRegistered = userRepository.existsByUsername(userName);
+
+        if (isEmailRegistered && isUserNameRegistered) {
+            throw new DuplicateEmailException("Both the email " + email.value() + " and username " + userName.value() + " are already registered. Please use different credentials.");
+        } else if (isEmailRegistered) {
+            throw new DuplicateEmailException("The email " + email + " is already registered. Please use a different email.");
+        } else if (isUserNameRegistered) {
+            throw new DuplicateEmailException("The username " + userName + " is already registered. Please use a different username.");
+        }
     }
 
 }
