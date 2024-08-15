ea771957caa74672873d27d74f5612b073f7669e
JuanC <abjuancarlos.12@gmail.com>
2024-08-15 00:44:23 -0500

    feat: create DomaintEvent, introduce  domain events our application. 😃

---
+++ src/main/java/com/bordify/shared/domain/bus/command/event/DomainEvent.java
@@ -0,0 +1,33 @@
+package com.bordify.shared.domain.bus.command.event;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public abstract class DomainEvent implements Serializable {
+
+    private String eventId;
+    private LocalDateTime occurredOn;
+
+    public abstract String eventName();
+
+    public DomainEvent() {
+        this.eventId     = UUID.randomUUID().toString();
+        this.occurredOn  = LocalDateTime.now();
+    }
+
+    public DomainEvent(String eventId, LocalDateTime occurredOn) {
+        this.eventId     = eventId;
+        this.occurredOn  = occurredOn;
+    }
+
+    public String eventId() {
+        return eventId;
+    }
+
+    public LocalDateTime occurredOn() {
+        return occurredOn;
+    }
+
+}
+
