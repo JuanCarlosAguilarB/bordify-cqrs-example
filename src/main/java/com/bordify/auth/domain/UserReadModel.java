b696657c881fa54c5b99c1d57322cb3593363441
JuanC <abjuancarlos.12@gmail.com>
2024-08-15 22:32:53 -0500

    feat: UserReadModel was updated.
    the changes was:
     - delete annotation @Data because it generates getters and setters for all properties (we can only access them via getters or via the constructor)
     - added annotations: EqualsAndHashCode and ToString
     - added constructor with all properties
     - (Important!)  deleted password field from UserReadModel, the authentication is via write model.

--- src/main/java/com/bordify/auth/domain/UserReadModel.java
+++ src/main/java/com/bordify/auth/domain/UserReadModel.java
@@ -3,19 +3,29 @@ package com.bordify.auth.domain;
 import com.bordify.shared.domain.UserUserId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
-@Data
-@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class UserReadModel {
 
     private final UserUserId userId;
     private final UserEmail email;
     private final UserUserName userName;
-    private final UserPassword password;
     private final UserIsVerified isVerified;
     private final UserDateCreated created;
     private final UserDateLastLogin lastLogin;
-        
+
+    public UserReadModel(UserUserId userId, UserEmail email, UserUserName userName, UserIsVerified isVerified, UserDateCreated created, UserDateLastLogin lastLogin) {
+        this.userId = userId;
+        this.email = email;
+        this.userName = userName;
+        this.isVerified = isVerified;
+        this.created = created;
+        this.lastLogin = lastLogin;
+    }
+
     public UserUserId id() {
         return userId;
     }
@@ -26,10 +36,6 @@ public class UserReadModel {
 
     public UserUserName userName() {
         return userName;
-    }
-
-    public UserPassword password() {
-        return password;
     }
 
     public UserIsVerified isVerified() {
