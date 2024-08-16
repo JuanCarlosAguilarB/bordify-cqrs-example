607d97a4d8616410b7ed83aea1fe51a74f5aeca7
JuanC <abjuancarlos.12@gmail.com>
2024-08-16 02:28:39 -0500

    feat: create entity for write model

---
+++ src/main/java/com/bordify/auth/infrastructure/persistence/UserWriteModelEntity.java
@@ -0,0 +1,31 @@
+package com.bordify.auth.infrastructure.persistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+//@Table(name = "\"user_auth_information\"")
+public class UserWriteModelEntity {
+
+    @Id
+    private UUID userId;
+    private String email;
+    private String userName;
+    private String password;
+    private Boolean isVerified;
+    private LocalDateTime created;
+    private LocalDateTime lastLogin;
+}
