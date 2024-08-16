a8948ddb305a46c77c779930b7a07c187e848c88
JuanC <abjuancarlos.12@gmail.com>
2024-08-16 02:37:14 -0500

    feat: create repository implementation for read model

---
+++ src/main/java/com/bordify/auth/infrastructure/persistence/UserReadRepositoryJpa.java
@@ -0,0 +1,51 @@
+package com.bordify.auth.infrastructure.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * JPA repository for the User entity.
+ * if we want to use JPA with Redis, we need to use CrudRepository instead of JpaRepository
+ * CrudRepository is a specialization of JpaRepository that adds additional methods for CRUD operations.
+ *
+ * JpaRepository does not support Redis, so we need to use CrudRepository instead.
+ */
+
+//public interface UserReadRepositoryJpa extends JpaRepository<UserReadModelEntity, UUID> {
+public interface UserReadRepositoryJpa extends CrudRepository<UserReadModelEntity, UUID> {
+
+    /**
+     * Checks if a user exists with the given username.
+     *
+     * @param userName The username of the user.
+     * @return True if a user exists with the given username, false otherwise.
+     */
+    public boolean existsByUserName(String userName);
+
+
+    /**
+     * Retrieves a user by their username.
+     *
+     * @param userName The username of the user.
+     * @return The user with the specified username, or null if not found.
+     */
+    public Optional<UserReadModelEntity> findByUserName(String userName);
+
+    /**
+     * Checks if a user exists with the given email.
+     *
+     * @return True if a user exists with the given email, false otherwise.
+     */
+    public boolean existsByEmail(String email);
+
+    /**
+     * Retrieves a user by their email address.
+     *
+     * @param email The email address of the user.
+     * @return The user with the specified email address, or null if not found.
+     */
+    public Optional<UserReadModelEntity> findByEmail(String email);
+
+}
--- src/main/java/com/bordify/auth/infrastructure/persistence/UserWriteModelEntity.java
+++ src/main/java/com/bordify/auth/infrastructure/persistence/UserWriteModelEntity.java
@@ -7,7 +7,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
