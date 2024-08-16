8693aedf8e401681c9a7114bff1228fdaee8a9fd
JuanC <abjuancarlos.12@gmail.com>
2024-08-16 02:31:36 -0500

    feat: create repository implementation for write model

---
+++ src/main/java/com/bordify/auth/infrastructure/persistence/UserWriteModelRepositoryJpaAdapter.java
@@ -0,0 +1,49 @@
+package com.bordify.auth.infrastructure.persistence;
+
+import com.bordify.auth.domain.UserEmail;
+import com.bordify.auth.domain.UserUserName;
+import com.bordify.auth.domain.UserWriteModel;
+import com.bordify.auth.domain.UserWriteModelRepository;
+import com.bordify.auth.infrastructure.mapper.UserWriteModelMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static com.bordify.auth.infrastructure.mapper.UserWriteModelMapper.toEntity;
+
+@Repository
+@AllArgsConstructor
+public class UserWriteModelRepositoryJpaAdapter implements UserWriteModelRepository {
+
+    private final UserWriteRepositoryJpa repository;
+
+    @Override
+    public boolean existsByUsername(UserUserName username) {
+        return repository.existsByUserName(username.value());
+    }
+
+    @Override
+    public void save(UserWriteModel user) {
+        repository.save(toEntity(user));
+    }
+
+    @Override
+    public Optional<UserWriteModel> findByUsername(UserUserName username)  {
+        return repository.findByUserName(username.value())
+                .map(UserWriteModelMapper::toDomain);
+    }
+
+    @Override
+    public boolean existsByEmail(UserEmail email) {
+        return repository.existsByEmail(email.value());
+    }
+
+    @Override
+    public Stream<UserWriteModel> findByEmail(UserEmail email) {
+        return repository.findByEmail(email.value()).stream()
+                .map(UserWriteModelMapper::toDomain);
+    }
+
+}
