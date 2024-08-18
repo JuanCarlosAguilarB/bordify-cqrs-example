ae3dd7895c77fa82f0a9d5620447294ad7b3a346
JuanC <abjuancarlos.12@gmail.com>
2024-08-18 18:51:56 -0500

    feat: AuthPostController is refactored. The appropriate commands and queries are created.

--- src/main/java/com/bordify/auth/infrastructure/controllers/AuthPostController.java
+++ src/main/java/com/bordify/auth/infrastructure/controllers/AuthPostController.java
@@ -1,7 +1,11 @@
 package com.bordify.auth.infrastructure.controllers;
 
-import com.bordify.auth.application.authenticate.UserAuthenticator;
-import com.bordify.auth.domain.Auth;
+import com.bordify.auth.application.authenticate.ObtainTokenUserQuery;
+import com.bordify.auth.application.authenticate.UserCreadentialsValidateCommand;
+import com.bordify.auth.application.update.UserLoginCommand;
+import com.bordify.auth.domain.TokenUserResponse;
+import com.bordify.shared.domain.bus.command.CommandBus;
+import com.bordify.shared.domain.bus.query.QueryBus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -10,13 +14,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @Tag(name = "Auth", description = "Authentication operations")
 @AllArgsConstructor
 public class AuthPostController {
 
-    private final UserAuthenticator userAuthenticator;
-
+    private final CommandBus commandBus;
+    private final QueryBus queryBus;
     /**
      * Authenticates a user and returns a JWT token.
      *
@@ -25,9 +31,25 @@ public class AuthPostController {
      */
     @Operation(summary = "Authenticate a user", description = "Authenticates a user and returns a JWT token", tags = {"Auth"})
     @PostMapping("/v1/login/")
-    public ResponseEntity<?> authenticateUser(@RequestBody Auth loginRequest) {
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
 
-        return ResponseEntity.ok(userAuthenticator.authenticate(loginRequest));
+        String username = loginRequest.getUserName();
+        String password = loginRequest.getPassword();
+
+        // Validate the credentials
+        UserCreadentialsValidateCommand command = new UserCreadentialsValidateCommand(username, password);
+        commandBus.send(command);
+
+        // Obtain the token
+        ObtainTokenUserQuery query = new ObtainTokenUserQuery(username);
+        TokenUserResponse response =  queryBus.ask(query);
+
+        // Update the user login date
+        // this command could have all the information needed, for example, ip address, user agent, etc.
+        LocalDateTime loginDate = LocalDateTime.now();
+        commandBus.send(new UserLoginCommand(username, loginDate));
+
+        return ResponseEntity.ok(response);
 
     }
 
@@ -45,4 +67,21 @@ public class AuthPostController {
 //        );
 //    }
 
+}
+
+class LoginRequest {
+    private final String userName;
+    private final String password;
+
+    public LoginRequest(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;    }
+
+    public String getPassword() {
+        return password;
+    }
 }
