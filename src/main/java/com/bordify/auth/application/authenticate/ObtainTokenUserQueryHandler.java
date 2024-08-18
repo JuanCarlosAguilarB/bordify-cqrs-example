8424e8128181dbfa23272a4ae48c7e56c83f1af7
JuanC <abjuancarlos.12@gmail.com>
2024-08-18 18:51:16 -0500

    feat: query handler was created to validate obtain token of user.

---
+++ src/main/java/com/bordify/auth/application/authenticate/ObtainTokenUserQueryHandler.java
@@ -0,0 +1,21 @@
+package com.bordify.auth.application.authenticate;
+
+import com.bordify.auth.domain.AuthServices;
+import com.bordify.auth.domain.TokenUserResponse;
+import com.bordify.auth.domain.UserUserName;
+import com.bordify.shared.domain.bus.query.QueryHandler;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ObtainTokenUserQueryHandler implements QueryHandler<ObtainTokenUserQuery, TokenUserResponse> {
+
+    private final AuthServices authServices;
+
+    @Override
+    public TokenUserResponse handle(ObtainTokenUserQuery query) {
+        UserUserName userName = new UserUserName(query.userName);
+        return authServices.createToken(userName);
+    }
+}
