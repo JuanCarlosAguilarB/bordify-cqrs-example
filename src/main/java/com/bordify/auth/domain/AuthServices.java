c96a633b76c2b9bb95762b274f6797b01a1213fa
JuanC <abjuancarlos.12@gmail.com>
2024-08-18 18:34:07 -0500

    feat: refactor service interface that managements token

--- src/main/java/com/bordify/auth/domain/AuthServices.java
+++ src/main/java/com/bordify/auth/domain/AuthServices.java
@@ -1,14 +1,12 @@
 package com.bordify.auth.domain;
 
-import com.bordify.shared.domain.CreadentialsNotValidException;
-
 public interface AuthServices {
 
-    public TokenUserResponse createToken(UserWriteModel user);
+    public TokenUserResponse createToken(UserUserName userName);
 
     public UserWriteModel decode(TokenUserResponse token);
 
-    public void ensureCredentialsAreValid(Auth auth) throws CreadentialsNotValidException;
+    public void ensureCredentialsAreValid(UserUserName userName, UserPassword password) throws CreadentialsNotValidException;
 
     public Boolean isValidToken(String token, String username);
 }
