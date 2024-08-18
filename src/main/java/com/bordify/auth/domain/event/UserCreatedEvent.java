ee47688b23d228f393c38155f21ea26e417858be
JuanC <abjuancarlos.12@gmail.com>
2024-08-18 03:55:19 -0500

    fix: added constructor by default, thi was a mistake because were using the default constructor by create a new instance of the class by reflexion.

--- src/main/java/com/bordify/auth/domain/event/UserCreatedEvent.java
+++ src/main/java/com/bordify/auth/domain/event/UserCreatedEvent.java
@@ -3,12 +3,14 @@ package com.bordify.auth.domain.event;
 import com.bordify.shared.domain.bus.event.DomainEvent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserCreatedEvent extends DomainEvent {
 
     private UUID userId;
