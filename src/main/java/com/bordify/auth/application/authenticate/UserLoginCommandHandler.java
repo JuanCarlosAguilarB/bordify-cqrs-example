ba9f8573ebca29febd1273a8c4464dc8f94c5a9c
JuanC <abjuancarlos.12@gmail.com>
2024-08-18 05:23:21 -0500

    feat: implement call to services for update last login

--- src/main/java/com/bordify/auth/application/authenticate/UserLoginCommandHandler.java
+++ src/main/java/com/bordify/auth/application/authenticate/UserLoginCommandHandler.java
@@ -1,5 +1,8 @@
 package com.bordify.auth.application.authenticate;
 
+import com.bordify.auth.application.update.LastLoginDateUpdater;
+import com.bordify.auth.domain.UserDateLastLogin;
+import com.bordify.shared.domain.UserUserId;
 import com.bordify.shared.domain.bus.command.CommandHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,9 +11,15 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UserLoginCommandHandler implements CommandHandler<UserLoginCommand> {
 
+    private final LastLoginDateUpdater lastLoginDateUpdater;
 
     @Override
     public void handle(UserLoginCommand command) {
+
+        UserUserId userId = new UserUserId(command.getUserId());
+        UserDateLastLogin lastLoginDate = new UserDateLastLogin(command.getLoginDate());
+
+        lastLoginDateUpdater.update(userId, lastLoginDate);
 
     }
 }
