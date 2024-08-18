fd222eb971fc42d0583193d2f9e781e972391287
JuanC <abjuancarlos.12@gmail.com>
2024-08-18 05:36:13 -0500

    feat: Rename class AuthenticationToken to TokenUserResponse, now this is a response object.

--- src/main/java/com/bordify/auth/domain/TokenUserResponse.java
+++ src/main/java/com/bordify/auth/domain/TokenUserResponse.java
@@ -1,11 +1,12 @@
 package com.bordify.auth.domain;
 
+import com.bordify.shared.domain.bus.query.Response;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class AuthenticationToken {
+public class TokenUserResponse implements Response {
 
     private String token;
     private String refreshToken;
