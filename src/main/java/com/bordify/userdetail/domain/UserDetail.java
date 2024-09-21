22299fd7a3485a9f1ea567ffb003c36997ff1ea8
JuanC <abjuancarlos.12@gmail.com>
2024-09-20 20:18:24 -0500

    fex: fixed use of UserUserId, we needed UserUserName.

--- src/main/java/com/bordify/userdetail/domain/UserDetail.java
+++ src/main/java/com/bordify/userdetail/domain/UserDetail.java
@@ -1,6 +1,7 @@
 package com.bordify.userdetail.domain;
 
 import com.bordify.shared.domain.UserUserId;
+import com.bordify.shared.domain.UserUserName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,7 +20,7 @@ public class UserDetail {
     private UserDetailFirstName firstName;
     private UserDetailLastName lastName;
     private UserDetailPhoneNumber phoneNumber;
-    private UserUserId username;
+    private UserUserName userName;
 
     public UserDetailId id() {
         return id;
@@ -37,7 +38,7 @@ public class UserDetail {
         return phoneNumber;
     }
 
-    public UserUserId username() {
-        return username;
+    public UserUserName userName() {
+        return userName;
     }
 }
