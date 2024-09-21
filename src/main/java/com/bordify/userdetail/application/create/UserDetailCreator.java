6e29b139a9e9f1b412ea5fa5bf915d700a2a32d0
JuanC <abjuancarlos.12@gmail.com>
2024-09-20 20:41:02 -0500

    feat: modify services to receive VO and build the domain object UserDetail.

--- src/main/java/com/bordify/userdetail/application/create/UserDetailCreator.java
+++ src/main/java/com/bordify/userdetail/application/create/UserDetailCreator.java
@@ -1,8 +1,8 @@
 package com.bordify.userdetail.application.create;
 
 import com.bordify.auth.domain.DuplicateEmailException;
-import com.bordify.userdetail.domain.UserDetail;
-import com.bordify.userdetail.domain.UserDetailRepository;
+import com.bordify.shared.domain.UserUserName;
+import com.bordify.userdetail.domain.*;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,8 +22,14 @@ public class UserDetailCreator {
      * @param user The user object containing the user details to be created.
      * @throws DuplicateEmailException If the provided email or username is already registered.
      */
-    public void createUser(UserDetail user) {
-
+    public void createUser(
+            UserDetailId id,
+            UserDetailFirstName firstName,
+            UserDetailLastName lastName,
+            UserDetailPhoneNumber phoneNumber,
+            UserUserName username
+    ) {
+        UserDetail user = new UserDetail(id, firstName, lastName, phoneNumber, username);
         userDetailRepository.save(user);
 
     }
