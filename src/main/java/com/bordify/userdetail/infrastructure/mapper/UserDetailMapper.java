60ba7798c20e2e551c0ade81204dc11444edac72
JuanC <abjuancarlos.12@gmail.com>
2024-09-20 20:34:43 -0500

    fix: fixed mapper that interact between UserDetail and UserDetailEntity

--- src/main/java/com/bordify/userdetail/infrastructure/mapper/UserDetailMapper.java
+++ src/main/java/com/bordify/userdetail/infrastructure/mapper/UserDetailMapper.java
@@ -1,30 +1,31 @@
 package com.bordify.userdetail.infrastructure.mapper;
 
-import com.bordify.userdetail.domain.UserDetail;
+import com.bordify.shared.domain.UserUserName;
+import com.bordify.userdetail.domain.*;
 import com.bordify.userdetail.infrastructure.persistence.UserDetailEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailMapper {
 
-    public static UserDetail toDomain(UserDetailEntity userDetailEntity) {
-        return UserDetail.builder()
-                .id(userDetailEntity.getId())
-                .username(userDetailEntity.getUsername())
-                .firstName(userDetailEntity.getFirstName())
-                .lastName(userDetailEntity.getLastName())
-                .phoneNumber(userDetailEntity.getPhoneNumber())
-                .build();
+    public static UserDetail toDomain(UserDetailEntity entity) {
+        return new UserDetail(
+                new UserDetailId(entity.getId()),
+                new UserDetailFirstName(entity.getFirstName()),
+                new UserDetailLastName(entity.getLastName()),
+                new UserDetailPhoneNumber(entity.getPhoneNumber()),
+                new UserUserName(entity.getUsername())
+        );
     }
 
 
     public static UserDetailEntity toEntity(UserDetail user) {
         return UserDetailEntity.builder()
-                .id(user.getId())
-                .username(user.getUsername())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .phoneNumber(user.getPhoneNumber())
+                .id(user.id().value())
+                .username(user.userName().value())
+                .firstName(user.firstName().value())
+                .lastName(user.lastName().value())
+                .phoneNumber(user.phoneNumber().value())
                 .build();
     }
 
