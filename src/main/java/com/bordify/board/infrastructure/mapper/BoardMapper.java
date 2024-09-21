e5bde7c44f47883705b0f1443b7f375d5902543d
JuanC <abjuancarlos.12@gmail.com>
2024-09-20 23:12:43 -0500

    feat: it changes using of user by userId; we want to reduce coupling.
    Also, if we dont do it, we can find an user in each service of board, that mean more latency.

--- src/main/java/com/bordify/board/infrastructure/mapper/BoardMapper.java
+++ src/main/java/com/bordify/board/infrastructure/mapper/BoardMapper.java
@@ -4,6 +4,7 @@ import com.bordify.board.domain.Board;
 import com.bordify.board.domain.BoardId;
 import com.bordify.board.domain.BoardName;
 import com.bordify.board.infrastructure.persistence.BoardEntity;
+import com.bordify.shared.domain.UserUserId;
 import com.bordify.userdetail.domain.UserDetail;
 import com.bordify.userdetail.infrastructure.mapper.UserDetailMapper;
 
@@ -11,11 +12,10 @@ public class BoardMapper {
 
     public Board toDomain(BoardEntity boardEntity) {
 
-        UserDetail user = UserDetailMapper.toDomain(boardEntity.getUser());
         return new Board(
                 new BoardId(boardEntity.getId()),
                 new BoardName(boardEntity.getName()),
-                user
+                new UserUserId(boardEntity.getUserId())
         );
     }
 
@@ -23,7 +23,7 @@ public class BoardMapper {
         return BoardEntity.builder()
                 .id(board.id().value())
                 .name(board.name().value())
-                .userId(board.user().id().value())
+                .userId(board.userId().value())
                 .build();
     }
 
